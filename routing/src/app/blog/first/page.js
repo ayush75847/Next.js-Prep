8461768e92c6c7bb073de8e421587c414495a613
Ayush Rawat <ayushrawat75847@gmail.com>
2026-09-26 15:28:26 +0530

    add nested routes

---
+++ routing/src/app/blog/first/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function First() {
+  return (
+    <div>First Blog Page</div>
+  )
+}
+
+export default First
---
+++ routing/src/app/blog/second/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Second() {
+  return (
+    <div>Second Blog Page</div>
+  )
+}
+
+export default Second
